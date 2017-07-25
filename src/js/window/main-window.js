c6da90c844ace8c350447eb081f005b9d5fae3e0
Eric Skogen <audionerd@users.noreply.github.com>
2017-07-25 12:50:05 -0500

    stats needs begin and end

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -167,9 +167,9 @@ const load = (event, args) => {
   stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
   document.body.appendChild(stats.dom)
   function animateStats () {
-  	// stats.begin()
-  	// // monitored code goes here
-  	// stats.end()
+  	stats.begin()
+  	// monitored code goes here
+  	stats.end()
   	requestAnimationFrame(animateStats)
   }
   requestAnimationFrame(animateStats)
